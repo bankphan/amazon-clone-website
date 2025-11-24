3ed8f62c577d9944dad07b75130d983f2064f931
bkphan <bkphan.me@gmail.com>
2025-11-24 14:27:18 +0700

    feat: Create the 'product-slider' section for homepage

--- js/script.js
+++ js/script.js
@@ -29,3 +29,11 @@ next_btn.addEventListener('click', (e)=>{
     }
     changeSlide();
 });
+
+const srcollContainer = document.querySelectorAll('.products');
+for (const item of srcollContainer) {
+    item.addEventListener('wheel', (evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    }
+}
